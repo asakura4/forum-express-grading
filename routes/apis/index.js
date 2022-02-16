b00478cc64a75ee076c149636cd9ef78ab74ef96
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-02-15 23:43:44 -0800

    add admin-services and getRestaurants

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const restaurantController = require('../../controllers/apis/restaurant-controller')
+const { generalErrorHandler } = require('../../middleware/error-handler')
+const passport = require('../../config/passport')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/restaurants', restaurantController.getRestaurants)
 
 module.exports = router
